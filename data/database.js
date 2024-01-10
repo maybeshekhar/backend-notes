d426b410b3f94539ef17bb01ed54fc888c08c721
Chandrashekhar Jenamoni <shekharmaybe@gmail.com>
2024-01-11 00:21:07 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,6 +5,9 @@ export const connectDB = () => {
 
     mongoose.connect(process.env.MONGO_URI, {
     dbName: "backendapi",
-}).then(() => console.log("Database Connected")).catch((e) => console.log(e));
+}).then((c) => console.log(`Database Connected with ${c.connection.host}`)).catch((e) => console.log(e));
 
 }
+
+
+
