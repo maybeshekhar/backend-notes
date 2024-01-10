33e503ab6caf4ff88f530cb862c641511227d3cc
Chandrashekhar Jenamoni <shekharmaybe@gmail.com>
2024-01-11 00:53:30 +0530

    fixed something

--- data/database.js
+++ data/database.js
@@ -6,8 +6,4 @@ export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
     dbName: "backendapi",
 }).then((c) => console.log(`Database Connected with ${c.connection.host}`)).catch((e) => console.log(e));
-
-}
-
-
-
+};
